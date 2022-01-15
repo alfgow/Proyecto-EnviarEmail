2b7185b471320188f748e332f2fc5dcaf228f5ce
alfonsovillanueva <alfonso@villanuevagarcia.com>
2022-01-14 19:57:47 -0600

    Proyecto Finalizado

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let enviar = document.querySelector("#enviar");
 let email = document.querySelector("#email");
 let asunto = document.querySelector("#asunto");
 let mensaje = document.querySelector("#mensaje");
+let validarMail;
 
 //! Event Listener
 addEventListeners();
@@ -14,26 +15,66 @@ function addEventListeners() {
 	email.addEventListener("blur", validarCampos);
 	asunto.addEventListener("blur", validarCampos);
 	mensaje.addEventListener("blur", validarCampos);
+	enviar.addEventListener("click", enviarFormulario);
 }
 
 //! Funciones
 
 // Iniciando App
-function iniciarApp() {
+function iniciarApp(e) {
 	enviar.disabled = "true";
 }
 
 // Validando Campos
 function validarCampos(e) {
+	const tipoDato = e.target.type;
 	if (e.target.value.length > 0) {
-		e.target.style.borderColor = "green";
+		campoConValor(e);
+		if (tipoDato === "email") {
+			const er =
+				/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			const testMail = er.test(e.target.value);
+			if (testMail) {
+				validarMail = 1;
+			} else {
+				campoVacio(e);
+
+				Swal.fire({
+					icon: "error",
+					title: "Error...",
+					text: `El correo '${e.target.value}' es incorrecto`,
+				});
+			}
+		}
 	} else {
-		let tipoCampo = e.target.id;
-		e.target.style.borderColor = "red";
-		Swal.fire({
-			icon: "error",
-			title: "Error...",
-			text: `Es necesario introducir un ${tipoCampo}`,
-		});
+		campoVacio(e);
 	}
+	if (asunto.value !== "" && mensaje.value !== "" && validarMail === 1) {
+		enviar.classList.remove("cursor-not-allowed", "opacity-50");
+		enviar.removeAttribute("disabled");
+	}
+}
+
+function campoConValor(e) {
+	e.target.style.borderColor = "green";
+}
+
+function campoVacio(e) {
+	let tipoCampo = e.target.id;
+	e.target.style.borderColor = "red";
+	Swal.fire({
+		icon: "error",
+		title: "Error...",
+		text: `Es necesario introducir un ${tipoCampo}`,
+	});
+}
+
+// Enviar email
+function enviarFormulario(e) {
+	e.preventDefault();
+	Swal.fire({
+		icon: "success",
+		title: "Todo Perfecto",
+		text: `Se ha enviado el correo`,
+	});
 }
